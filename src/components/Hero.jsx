c7e02edef3351796127f7171c9299b9aa43626e5
davidsanchez222 <david100sanchez2003@gmail.com>
2024-03-30 16:54:25 -0400

    small

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">David</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm and <br className="sm:block hidden" />
+            I'm an aspiring data engineer and <br className="sm:block hidden" />
             full stack developer.
           </p>
         </div>
